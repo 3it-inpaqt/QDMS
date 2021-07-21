9fed0892e97cc606749aafe61e5f05d5cdb2667d
Talgarr <sedragon1@gmail.com>
2021-07-21 11:18:31 -0400

    Update PulsedProgramming.py

--- qdms/PulsedProgramming.py
+++ qdms/PulsedProgramming.py
@@ -184,8 +184,8 @@ class PulsedProgramming:
         for key in diff_voltage.keys():
             print(f'{round(key*1000, 4)} mV\t{diff_voltage.get(key)}')
 
-        print(f'Mean diff: {np.mean(list(diff_voltage.key()))}')
-        print(f'Min diff: {np.min(list(diff_voltage.key()))}\tMax diff: {np.max(list(diff_voltage.key()))}')
+        print(f'Mean diff: {np.mean(list(diff_voltage.keys()))}')
+        print(f'Min diff: {np.min(list(diff_voltage.keys()))}\tMax diff: {np.max(list(diff_voltage.key()))}')
 
     def simulate_list_memristor(self, list_resistance):
         for i in range(self.memristor_simulation.circuit.number_of_memristor):
