c722ba23c6d5faad90e85314400b22483254c2c3
Talgarr <sedragon1@gmail.com>
2021-07-26 15:25:04 -0400

    up

--- qdms/MemristorSimulation.py
+++ qdms/MemristorSimulation.py
@@ -189,7 +189,7 @@ class MemristorSimulation:
 
         for i in range(self.circuit.number_of_memristor):
             self.circuit.list_memristor[i].g = 1 / current_res[i]
-        self.voltages_memristor[self.circuit.current_v_out()] = [1/i.g for i in self.circuit.list_memristor]
+        self.voltages_memristor[round(self.circuit.current_v_out(), 10)] = [round(1/i.g, 3) for i in self.circuit.list_memristor]
 
     def loop_rec(self, list_resistance, counter, current_states):
         """
