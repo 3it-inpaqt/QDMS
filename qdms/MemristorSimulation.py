6fd654600d7326a51bdcfa487277aefa8085a942
Talgarr <sedragon1@gmail.com>
2021-07-27 15:56:26 -0400

    Update MemristorSimulation.py

--- qdms/MemristorSimulation.py
+++ qdms/MemristorSimulation.py
@@ -141,8 +141,7 @@ class MemristorSimulation:
             print()
 
         self.voltages_memristor_dict = {k: self.voltages_memristor_dict[k] for k in sorted(self.voltages_memristor_dict)}
-
-        self.voltages_memristor = np.array([[k, list(v)] for k, v in self.voltages_memristor_dict.items()])
+        self.voltages_memristor = np.array([[k, np.array(v)] for k, v in self.voltages_memristor_dict.items()])
 
         self.voltages_memristor_dict.clear()
         for i in self.circuit.list_memristor:
