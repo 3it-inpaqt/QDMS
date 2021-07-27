47a032476301803b616b7dc3a06310604e6ec9ce
Talgarr <sedragon1@gmail.com>
2021-07-27 12:04:00 -0400

    Update Log.py

--- qdms/Log.py
+++ qdms/Log.py
@@ -171,7 +171,7 @@ def save_memristor_simulation_hdf5(memristor_sim, path):
         f.create_dataset("distribution_type", data=memristor_sim.distribution_type)
         voltages_memristor = f.create_group("voltages_memristor")
         for k, v in memristor_sim.voltages_memristor.items():
-            voltages_memristor.create_dataset(k, data=v)
+            voltages_memristor.create_dataset(str(k), data=v)
         f.create_dataset("verbose", data=memristor_sim.verbose)
         f.create_dataset("list_resistance", data=memristor_sim.list_resistance)
         f.create_dataset("timers", data=memristor_sim.timers)
